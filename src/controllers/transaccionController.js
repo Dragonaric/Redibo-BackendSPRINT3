97dbf8e67c0573c700aeeee978723e2674e1186b
Marko 3 <marcosvelasquezvela123@gmail.com>
2025-05-31 16:18:01 -0400

    fix: error en saldo

--- src/controllers/transaccionController.js
+++ src/controllers/transaccionController.js
@@ -1,10 +1,8 @@
-
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getTransacciones = async (req, res) => {
   try {
-
     const transacciones = await prisma.transaccion.findMany({
       orderBy: { createdAt: "desc" },
       where: { estado: "PENDIENTE" },
@@ -14,25 +12,22 @@ const getTransacciones = async (req, res) => {
             nombre: true,
             foto: true,
             saldo: true,
-
           },
         },
       },
     });
 
     return res.json(transacciones);
-  } catch (error) { 
+  } catch (error) {
     console.error("Error al obtener transacciones:", error);
     return res.status(500).json({ error: "Error del servidor" });
-  } 
-
-}
+  }
+};
 
 const aceptarTransaccion = async (req, res) => {
   const { id } = req.params;
 
   try {
-
     const transaccion = await prisma.transaccion.findUnique({
       where: { id: id },
       select: {
@@ -42,10 +37,10 @@ const aceptarTransaccion = async (req, res) => {
       },
     });
 
-    if (!transaccion) 
+    if (!transaccion)
       return res.status(404).json({ error: "Transacción no encontrada" });
 
-    if(transaccion.tipo === "SUBIDA") {
+    if (transaccion.tipo === "SUBIDA") {
       await prisma.usuario.update({
         where: { id: transaccion.userId },
         data: {
@@ -61,7 +56,10 @@ const aceptarTransaccion = async (req, res) => {
           estado: "COMPLETADA",
         },
       });
-      return res.json({ mensaje: "Transacción aceptada exitosamente", transaccion });
+      return res.json({
+        mensaje: "Transacción aceptada exitosamente",
+        transaccion,
+      });
     }
 
     await prisma.transaccion.update({
@@ -71,13 +69,15 @@ const aceptarTransaccion = async (req, res) => {
       },
     });
 
-    return res.json({ mensaje: "Transacción aceptada exitosamente", transaccion });
-  }
-  catch (error) {
+    return res.json({
+      mensaje: "Transacción aceptada exitosamente",
+      transaccion,
+    });
+  } catch (error) {
     console.error("Error al aceptar transacción:", error);
-    return res.status(500).json({ error: "Error del servidor" }); 
+    return res.status(500).json({ error: "Error del servidor" });
   }
-}
+};
 
 const rechazarTransaccion = async (req, res) => {
   const { id } = req.params;
@@ -89,29 +89,23 @@ const rechazarTransaccion = async (req, res) => {
         userId: true,
         monto: true,
         tipo: true,
-      }
+      },
     });
 
-    if (!transaccion) 
+    if (!transaccion)
       return res.status(404).json({ error: "Transacción no encontrada" });
 
-    if(transaccion.tipo === "SUBIDA") {
+    if (transaccion.tipo === "SUBIDA") {
       await prisma.transaccion.update({
         where: { id: id },
         data: {
           estado: "RECHAZADA",
         },
       });
-
-      await prisma.usuario.update({
-        where: { id: transaccion.userId },
-        data: {
-          saldo: {
-            decrement: transaccion.monto,
-          },
-        },
+      return res.json({
+        mensaje: "Transacción rechazada exitosamente",
+        transaccion,
       });
-      return res.json  ({ mensaje: "Transacción rechazada exitosamente", transaccion });
     }
 
     await prisma.transaccion.update({
@@ -128,13 +122,14 @@ const rechazarTransaccion = async (req, res) => {
         },
       },
     });
-    return res.json({ mensaje: "Transacción rechazada exitosamente", transaccion });
-
-  }
-  catch (error) {
+    return res.json({
+      mensaje: "Transacción rechazada exitosamente",
+      transaccion,
+    });
+  } catch (error) {
     console.error("Error al rechazar transacción:", error);
-    return res.status(500).json({ error: "Error del servidor" }); 
+    return res.status(500).json({ error: "Error del servidor" });
   }
-}
+};
 
 module.exports = { getTransacciones, aceptarTransaccion, rechazarTransaccion };
