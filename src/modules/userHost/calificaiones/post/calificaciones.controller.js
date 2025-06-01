6726c81583fac6857cfcd1b18aaef0d8704e8d2b
Edmilzon <edmilzon.luna@gmail.com>
2025-06-01 13:24:01 -0400

    sad

---
+++ src/modules/userHost/calificaiones/post/calificaciones.controller.js
@@ -0,0 +1,19 @@
+const {Router} = require('express');
+
+const router = Router();
+router.post('/', async (req, res) => {
+    const { id_host, id_renter, calificacion, fecha } = req.body;
+    try {
+        const nuevaCalificacion = await calificaionService.create({
+            id_host,
+            id_renter,
+            calificacion
+        });
+        res.status(201).json(nuevaCalificacion);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error al registrar la calificación' });
+    }
+});
+
+module.exports = router;
---
+++ src/modules/userHost/calificaiones/post/calificaciones.service.js
@@ -0,0 +1,20 @@
+const prisma = require('../../../../config/database');
+
+const comentarioHost = {
+
+    async create(data) {
+        try {
+            return await prisma.calificacionhost.create({
+                data: {
+                    id_host: data.id_host,
+                    id_renter: data.id_renter,
+                    calificacion: data.calificacion,
+                }
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+};
+
+module.exports = comentarioHost;
